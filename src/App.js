bfba302911c0e46a48e5613ab822e95140d66de5
ninjacanthi1995 <hoangminhchaucp54@gmail.com>
2021-12-26 23:56:34 +0100

    Add html structure, fetch all countries

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [countriesData, setCountriesData] = useState([])
+    const [filteredCountriesData, setFilteredCountriesData] = useState([])
+
+    let countries = filteredCountriesData.map(country => (
+        <div>
+            <img src={country.flag} />
+            <div>
+                <h2>{country.name}</h2>
+                <p><span>Population:</span> {country.population}</p>
+                <p><span>Region:</span> {country.region}</p>
+                <p><span>Capital:</span> {country.capital}</p>
+            </div>
+        </div>
+    ))
+
+    useEffect(() => {
+        fetch('https://restcountries.com/v2/all')
+            .then(response => response.json())
+            .then(data => {
+                setCountriesData(data)
+                setFilteredCountriesData(data)
+            });
+    }, [])
+
+    return (
+        <div className="App">
+            <header className="flex">
+                <p>Where in the world?</p>
+                <div className="flex">
+                    <img src='./sleep-mode.png' alt="dark mode" />
+                    <p>Dark Mode</p>
+                </div>
+            </header>
+
+            <main className="flex-col">
+                <div>
+                    <input />
+
+                    <select>
+                        <option>Filter by Region</option>
+                    </select>
+                </div>
+
+                <section className="flex">
+                    {countries}
+                </section>
+            </main>
+        </div>
+    );
 }
 
 export default App;
